dbe4742caf753deedfb2e5c7390ab0d4ee74df48
Tamim Ahasan Rijon <tamimahsan67@gmail.com>
2024-05-22 00:20:23 +0600

    excercise

---
+++ excercise/array.java
@@ -0,0 +1,7 @@
+public class array {
+    public static void main(String[] args) {
+        int number_arr[] = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println("Array elements are: "+ number_arr[0]);
+    }
+}
