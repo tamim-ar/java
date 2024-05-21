34c982112b1d94976a61a927bc680aa3828a9174
Tamim Ahasan Rijon <tamimahsan67@gmail.com>
2024-05-22 01:44:10 +0600

    night end

---
+++ excercise/variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int result = a + b;
+
+        System.out.println(result);
+    }
+}
