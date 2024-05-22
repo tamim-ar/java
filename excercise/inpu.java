bfad678fcbce7dfed3e9ac561e7e1f681cefe5da
Tamim Ahasan Rijon <tamimahsan67@gmail.com>
2024-05-22 13:08:06 +0600

    up

--- excercise/inpu.java
+++ excercise/inpu.java
@@ -1,6 +1,6 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class input {
+public class inpu {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
