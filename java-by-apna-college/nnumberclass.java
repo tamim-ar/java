8244fc3dfc870e3d5b547db889b4c35bcfab2fa6
Tamim Ahasan Rijon <tamimahsan67@gmail.com>
2024-05-22 13:41:56 +0600

    mid

---
+++ java-by-apna-college/nnumberclass.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class nnumberclass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int i = 0, n = sc.nextInt();
+
+        for (i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ java-by-apna-college/whilelloop.java
@@ -0,0 +1,9 @@
+public class whilelloop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i+" Hello World");
+            i++;
+        }
+    }
+}
