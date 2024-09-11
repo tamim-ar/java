e6120288200c6703790f960f46a76bbe9e1ca043
Tamim Ahasan Rijon <tamimahsan.ar@gmail.com>
2024-09-11 16:00:31 +0600

    up

---
+++ alpha-coding-batch/variable1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class variable1{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+// String input = sc.next();
+String input = sc.nextLine();
+System.out.println(input);
+}
+}
---
+++ java-by-apna-college/pin.java
@@ -0,0 +1,5 @@
+public class pin{
+public static void main(String args[]){
+System.out.println("Tamim Ahasan");
+}
+}
