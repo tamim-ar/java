38f55155cb9a4839c5e1609b0817d10df268cdd1
Tamim Ahasan Rijon <tamimahsan.ar@gmail.com>
2024-09-11 16:00:49 +0600

    up

--- dsa/demo.java
+++ dsa/demo.java
@@ -1,17 +1,15 @@
 public class demo {
-    public static void main (String[] args) {
-        int nums[] = {1, 2, 3, 4, 5};
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 3, 4, 5 };
         int target = 5;
 
         int result = linearSearch(nums, target);
 
         System.out.println("Element found at index: " + result);
     }
-public static int linearSearch(int[] nums, int target) {
 
+    public static int linearSearch(int[] nums, int target) {
 
-
-
-    return -1;
-}
+        return -1;
+    }
 }
