be045f17852f6b45b37a1be8523a7a4373e0ec96
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-10-27 00:51:34 +0600

    Refactor casting.java and final_var.java files

---
+++ java-by-apna-college/casting.java
@@ -0,0 +1,10 @@
+public class casting {
+    public static void main(String[] args) {
+        double num1 = 18.999;
+        int num2 = 18;
+
+        int num3 = num2 + (int)num1;
+
+        System.out.println(num3);
+    }
+}
---
+++ java-by-apna-college/final_var.java
@@ -0,0 +1,8 @@
+public class final_var {
+    public static void main(String[] args) {
+        final float PI = 3.1416F;
+
+        // PI = 3.00;
+        System.out.println(PI);
+    }
+}
