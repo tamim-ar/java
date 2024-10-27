2a6d360ed82998a823c53d85fbdd3a05d8c8def6
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-10-28 00:59:00 +0600

    up

--- oop/two/Main.java
+++ oop/two/Main.java
@@ -3,7 +3,7 @@ import oop.bank.*;
 
 public class Main {
     public static void main(String[] args) {
-        bank.Account a1 = new bank.Account();
+        oop.bank.Account a1 = new oop.bank.Account();
         a1.name = "customer1";
     }
 }
