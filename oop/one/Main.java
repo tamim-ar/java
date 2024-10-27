412b66992a87fda045973b2fa4716885bac237a3
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-10-27 21:43:22 +0600

    oop

---
+++ oop/one/Main.java
@@ -0,0 +1,11 @@
+package oop.one;
+
+public class Main {
+    public static void main(String[] args) {
+        Pen pen1 = new Pen();
+        pen1.color = "Red";
+        pen1.type = "Gel";
+
+        pen1.write();
+    }
+}
---
+++ oop/one/Pen.java
@@ -0,0 +1,10 @@
+package oop.one;
+
+public class Pen {
+    String color; 
+    String type;  
+
+    public void write() {
+        System.out.println("Writing something with a " + color + " " + type + " pen.");
+    }
+}
