6051a1816367c8923d1a7fcd6b91db3afc78a879
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-10-28 01:58:37 +0600

    up

---
+++ linked-list/LinkedList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class LinkedList {
+public static void main(String[] args) {
+    LinkedList linky = new LinkedList();
+
+    linky.add("rob");
+    linky.add("Alex");
+    linky.add("jose");
+}
+}
