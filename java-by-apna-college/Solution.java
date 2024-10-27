9a850726be823321c8e30935bded789d7befca12
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-10-27 15:06:30 +0600

    up

---
+++ java-by-apna-college/Solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        
+        // String name1 = scan.nextLine();
+        // String name2 = scan.nextLine();
+        // String name3 = scan.nextLine();
+        
+        // System.out.println(1 + " " + name1);
+        // System.out.println(2 + " " + name2);
+        // System.out.println(3 + " " + name3);
+        
+        int no = 1;
+        while(scan.hasNext()){
+            System.out.println(no++ + " " + scan.nextLine());
+        }
+        scan.close();
+    }
+}
