0566d35ed8a11dafab10734bcd924b7f0cea3b7e
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-10-28 18:23:42 +0600

    ConsecutiveDuplicates

---
+++ problems/ConsecutiveDuplicates.java
@@ -0,0 +1,21 @@
+package problems;
+
+import java.util.*;
+
+public class ConsecutiveDuplicates {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+
+        StringBuilder name1 = new StringBuilder();
+        name1.append(name.charAt(0));
+
+        for (int i = 1; i < name.length(); i++) {
+            if (name.charAt(i) != name.charAt(i - 1)) {
+                name1.append(name.charAt(i));
+            }
+        }
+        System.out.println(name);
+        System.out.println(name1);
+    }
+}
