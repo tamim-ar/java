c6855bcc9ecbf76664b8fb2e64067ae9d80a52a6
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2024-10-29 06:00:18 +0600

    LeapYear

---
+++ problems/BirthYear.java
@@ -0,0 +1,30 @@
+package problems;
+
+public class BirthYear {
+    public static void main(String[] args) {
+        int BirthYear = 20034, BirthMonth = 10, BirthDate = 19;
+        int CurrentYear = 2024, CurrentMonth = 11, CurrentDate = 30;
+        int TotalYear, TotalMonth, TotalDate;
+
+        TotalYear = CurrentYear - BirthYear;
+        TotalMonth = CurrentMonth - BirthMonth;
+        TotalDate = CurrentDate - BirthDate;
+
+        if (BirthYear > CurrentYear || BirthYear == CurrentYear && BirthMonth > CurrentMonth || BirthYear == CurrentYear && CurrentMonth == CurrentMonth && BirthDate > CurrentDate || BirthMonth >= 12 || BirthDate >= 31 || CurrentMonth >= 12 || CurrentDate >= 31){
+            System.out.println("invalid date");
+            System.exit(0);
+        }
+
+        if (TotalDate < 0) {
+            TotalDate += 30;
+            TotalMonth -= 1;
+        }
+        if (TotalMonth < 0) {
+            TotalMonth += 12;
+            TotalYear -= 1;
+        }
+
+        System.out.println(TotalYear + " " + TotalMonth + " " + TotalDate);
+
+    }
+}
