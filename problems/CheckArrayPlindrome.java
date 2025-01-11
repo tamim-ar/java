1e19967d6385dd3644ce4ba13a1e3376e3c121f7
Tamim Ahasan Rijon <tamim15-3289@diu.edu.bd>
2025-01-11 13:48:38 +0600

    Fix array initialization in CheckArrayPlindrome to correctly represent a palindrome

--- problems/CheckArrayPlindrome.java
+++ problems/CheckArrayPlindrome.java
@@ -2,7 +2,7 @@ package problems;
 
 public class CheckArrayPlindrome {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 2, 1 };
+        int[] arr = { 1, 2, 3, 2, 1 };
         int left = 0;
         int right = arr.length - 1;
         while (left < right) {
